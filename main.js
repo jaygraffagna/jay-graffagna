a2f798f1e1101d407de2494c9b6a49129040b509
jaygraffagna <jaygraffagna10@gmail.com>
2021-08-22 13:13:29 -0500

    feat(init): Add existing site

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
-import './style.css'
+import html from "./html";
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+document.querySelector("#app").innerHTML = html`
+  <img src="./Images/profile.jpg" alt="Jay Graffagna Profile" />
+  <h1>Jay Graffagna</h1>
+  <p>
+    I'm a software developer who is passionate about sustainability. I currently
+    work at MidAmerican Energy in Des Moines, IA. I mostly build apps in C# (and
+    some in JS). Outside of work, I use javascript for my projects. I post some
+    code
+    <a href="https://github.com/jaygraffagna">here</a>. Feel free to contact me
+    <a href="mailto:jaygraffagna10@gmail.com">here</a>.
+  </p>
+`;
