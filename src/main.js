fa70f4cae75975142726d9dd16f455cc6154c6df
Jay Graffagna <jaygraffagna@gmail.com>
2021-09-27 08:53:40 -0500

    chore(content): Remove import

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import header from "./components/header";
 import footer from "./components/footer";
 import home from './pages/home';
 import projects from './pages/projects';
-import principles from './pages/principles';
 import about from './pages/about';
 
 document.querySelector("#app").innerHTML = `
