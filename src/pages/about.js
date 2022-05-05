6d2ec8df7d25cbcbe8e0c0a8d186a4fc1e406217
Jay Graffagna <jaygraffagna@gmail.com>
2022-05-05 14:18:26 -0500

    Update about

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,8 +7,7 @@ const about = () => {
       <p>
         My life is much more than my occupation and projects I've
         worked on. I'm a dad to a toddler, Sawyer, and married to my partner,
-        Abbey. We live in the Des Moines area. It's a cool small city with a lot
-        going on. When I am not at work, I spend a lot of my time away from screens 
+        Abbey. When I am not at work, I spend a lot of my time away from screens 
         spending time outside with my family.
       </p>
       <p>
@@ -18,18 +17,13 @@ const about = () => {
         the goal.
       </p>
       <p>
-        I am a lover of nature and the arts. I think a lot about what our
-        landscapes would be like in their natural state before the settlement of
-        humans. When people ask me what I would be if I wasn't a developer, I
+        I am a lover of nature and the arts. When people ask me what I would be if I wasn't a developer, I
         would be a naturalist. I am far from an expert on naturalism and native
         ecosystems, but I love learning from experts and participating in the
-        protection of native habitats. My partner Abbey is an artist and we
+        protection of native habitats. My partner Abbey is an artist/illustrator and we
         both have a passion for the arts. We like getting out and
-        seeing public art or connecting with the local art community. I have
-        dabbled a bit in photography and pastel paintings, but don't have much
-        to show for it. My all time favorite thing to do is go to live music. I
-        have been to many shows and they all kind of blur together (in a good
-        way). It has allowed me to experience some amazing artists and venues.
+        seeing public art or connecting with the local art community. My all time favorite thing to do is go to live music. I
+        have been to many shows over the years and it has allowed me to experience some amazing artists and venues.
       </p>
       <p>
         My hobbies include biking, hiking, and kayaking (really anything to get
